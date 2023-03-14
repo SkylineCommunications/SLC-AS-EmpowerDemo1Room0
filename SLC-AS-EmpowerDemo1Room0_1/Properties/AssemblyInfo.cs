60cab4ac106dc0a32a69aeea3e4ac7bead444944
Jan Staelens <jan.staelens@skyline.be>
2023-03-14 17:13:26 +0100

    Add project files.

---
+++ SLC-AS-EmpowerDemo1Room0_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SLC-AS-EmpowerDemo1Room0_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("SLC-AS-EmpowerDemo1Room0_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("247AF4E6-3F11-4D5C-A5EC-46162E0FC677")]
+[assembly: AssemblyVersion("1.0.0.0")]
